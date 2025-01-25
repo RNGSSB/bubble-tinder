16ee20599b70045ae5f604071e0eb960ca94ffdc
RNGSSB <59853617+RNGSSB@users.noreply.github.com>
2025-01-25 14:07:42 -0600

    Text stuff + state machine

--- bubble-tinder/marioteachestyping.gd
+++ bubble-tinder/marioteachestyping.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 @onready var playerText = $LineEdit
-@onready var labelAwesome = $Label
+@onready var labelAwesome = $EpicLabel
+@export var prompt = "Sonic Adventure 2 Battle"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,57 +15,50 @@ func _process(delta):
 
 
 func _on_line_edit_text_submitted(new_text):
-	print(new_text.length())
-	
-	var idk = ""
-	
-	if new_text.length() == labelAwesome.text.length():
-		print("Equal lenght!")
-		for i in new_text.length():
-			if new_text[i] == labelAwesome.text[i]:
-				print(" Character " + str(i) + "is correct.")
-			else:
-				idk += labelAwesome.text[i]
-				print(labelAwesome.text[i] + " Wrong, stupid.")
-			i+=1
-	elif new_text.length() > labelAwesome.text.length():
-		print("Input text is bigger than prompt")
-		var diff = labelAwesome.text.length() + 1
-		for i in labelAwesome.text.length():
-			if new_text[i] == labelAwesome.text[i]:
-				print(" Character " + str(i) + "is correct.")
-			else:
-				idk += labelAwesome.text[i]
-				print(labelAwesome.text[i] + " Wrong, stupid.")
-			i+=1
-		while diff < new_text.length():
-				print("Esto esta de mas :/ " + new_text[diff])
-				diff += 1
-	else:
-		print("Input text is smaller than prompt")
-		if new_text.length() > 0:
-			for i in new_text.length():
-				if new_text[i] == labelAwesome.text[i]:
-					print(" Character " + str(i) + "is correct.")
-				else:
-					idk += labelAwesome.text[i]
-					print(labelAwesome.text[i] + " Wrong, stupid.")
-				i+=1
-			var diff = new_text.length() + 1
-			while diff < labelAwesome.text.length():
-				print("Te falto " + labelAwesome.text[diff])
-				diff += 1
-		else:
-			var diff = new_text.length()
-			while diff < labelAwesome.text.length():
-				print("Te falto " + labelAwesome.text[diff])
-				diff += 1
-		
-	
-	
-	
-	if(new_text == labelAwesome.text):
+	if(new_text == prompt):
 		print("YES!")
 	else:
 		print("NO!")
-		print(idk)
+
+
+
+func _on_line_edit_text_changed(new_text):
+	var idk = ""
+
+	if new_text.length() == prompt.length():
+		for i in new_text.length():
+			if new_text[i] == prompt[i]:
+				idk += "[color=black]" + prompt[i] + "[/color]"
+			else:
+				idk += "[color=red]" + prompt[i] + "[/color]"
+			i+=1
+	elif new_text.length() > prompt.length():
+		var diff = prompt.length()
+		for i in prompt.length():
+			if new_text[i] == prompt[i]:
+				idk += "[color=black]" + prompt[i] + "[/color]"
+			else:
+				idk += "[color=red]" + prompt[i] + "[/color]"
+			i+=1
+		while diff < new_text.length():
+				idk += "[color=red]" + new_text[diff] + "[/color]"
+				diff += 1
+	else:
+		if new_text.length() > 0:
+			for i in new_text.length():
+				if new_text[i] == prompt[i]:
+					idk += "[color=black]" + prompt[i] + "[/color]"
+				else:
+					idk += "[color=red]" + prompt[i] + "[/color]"
+				i+=1
+			var diff = new_text.length()
+			while diff < prompt.length():
+				idk += "[color=gray]" + prompt[diff] + "[/color]"
+				diff += 1
+		else:
+			var diff = new_text.length()
+			while diff < prompt.length():
+				idk += "[color=gray]" + prompt[diff] + "[/color]"
+				diff += 1
+	
+	labelAwesome.text = idk
---
+++ bubble-tinder/scripts/game_system.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@onready var stateMachine = $StateMachine
+@export var stateLabel : RichTextLabel
+@export var timerLabel : RichTextLabel
+
+@export var PREVSTATE = "Setup"
+@export var CURRSTATE = "Setup"
+
+var stateFrame = 0
+var frameCounter = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	frameCounter = 0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	frameCounter += 1
+	timerLabel.text = str(frameCounter)
