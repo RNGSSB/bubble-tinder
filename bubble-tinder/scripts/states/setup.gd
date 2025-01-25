d16099f9053b0d113a34c070569c97ea3b1c44a7
Rimmo <rimmocm@gmail.com>
2025-01-25 17:27:08 -0600

    setup visibility

--- bubble-tinder/scripts/states/setup.gd
+++ bubble-tinder/scripts/states/setup.gd
@@ -7,6 +7,7 @@ func Enter():
 	owner.stateLabel.text = owner.CURRSTATE
 	# Start checking authority for the setup buttons
 	print("IM CHECKING THE AUTHORITY!")
+	$SETUP.visible = true
 	$SETUP.check_authority()
 	
 
