67786813ba8ad52ffc7300c6e6d137f10cad9439
RNGSSB <59853617+RNGSSB@users.noreply.github.com>
2025-01-25 15:15:34 -0600

    Update marioteachestyping.gd

--- bubble-tinder/marioteachestyping.gd
+++ bubble-tinder/marioteachestyping.gd
@@ -10,7 +10,7 @@ func _enter_tree():
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if is_multiplayer_authority():
-		playerText.editable = false
+		playerText.editable = true
 	else:
 		playerText.editable = false
 
