86c07af1f4e37b93032f07fbd71ffddbba3e7f0b
Rimmo <rimmocm@gmail.com>
2025-01-25 16:18:28 -0600

    multiplayer fixes

--- bubble-tinder/multiplayer_manager.gd
+++ bubble-tinder/multiplayer_manager.gd
@@ -1,27 +1,41 @@
 extends Node2D
 
 var peer = ENetMultiplayerPeer.new()
-@export var setup_scene: PackedScene
+@export var player_ID_scene: PackedScene
 
-var player_one = ""
-var player_two = ""
+var player_one 
+var player_two
+
+var current_player_ID = 0
 
 func _on_host_pressed():
 	peer.create_server(80)
 	multiplayer.multiplayer_peer = peer
-	multiplayer.peer_connected.connect(_add_player)
+	multiplayer.peer_connected.connect(_add_second_player)
 	_add_player()
 
-func _add_player(id = 1):
-	if player_one != "":
-		player_two = id
-	else:
-		player_one = id
+func _add_second_player(id = 1):
+	player_two = id
 	print("I've added player: " + str(id))
+	player_two = player_ID_scene.instantiate()
+	player_two.name = str(id)
+	player_two.set_id(id)
+	call_deferred("add_child", player_two)
+	_verify_has_two_players()
+	
+func _add_player(id = 1):
+	player_one = id
+	print("I've added player: " + str(id))
+	player_one = player_ID_scene.instantiate()
+	player_one.name = str(id)
+	player_one.set_id(id)
+	call_deferred("add_child", player_one)
+	_verify_has_two_players()
 	
 func _verify_has_two_players():
-	if player_one != "" and player_two != "":
+	if player_one != 0 and player_two != 0:
 		print("We have two players now!")
+	# add the player authority
 
 func _on_join_pressed():
 	peer.create_client("169.254.212.37", 80)
