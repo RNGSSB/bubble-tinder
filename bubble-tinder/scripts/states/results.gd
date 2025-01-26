2ea03ea8ac05e95cc36172a12e2b4dc3a3df17f7
RNGSSB <59853617+RNGSSB@users.noreply.github.com>
2025-01-26 02:54:42 -0600

    Traits

--- bubble-tinder/scripts/states/results.gd
+++ bubble-tinder/scripts/states/results.gd
@@ -7,6 +7,15 @@ func Exit():
 func Enter():
 	owner.stateLabel.text = owner.CURRSTATE
 	print("Results!!!!")
+	
+	if ScoreManager.player1score > ScoreManager.player2score:
+		print("Player 1 Wins!!!!!")
+	elif ScoreManager.player1score < ScoreManager.player2score:
+		print("Player 2 Wins!!!!!")
+	elif ScoreManager.player1score <= 0 and ScoreManager.player2score <= 0:
+		print("You both kind of suck...")
+	elif ScoreManager.player1score == ScoreManager.player2score:
+		print("You both kind of suck...")
 
 func Update(_delta: float):
 	pass
