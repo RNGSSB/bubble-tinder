efadac0e6a631bf2f932943c2a6427609267c036
Rimmo <rimmocm@gmail.com>
2025-01-25 18:59:58 -0600

    typing spawners fix

--- bubble-tinder/typing_spawner.gd
+++ bubble-tinder/typing_spawner.gd
@@ -11,17 +11,16 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _on_start_typing_callback() -> void:
-	_on_start_typing.rpc()
+	_on_start_typing.rpc(multiplayer.get_unique_id())
 	
 @rpc("call_local", "reliable", "any_peer")
 func _on_start_typing() -> void:
-	var typing = typing_scene.instantiate()
-	typing.name = str(multiplayer.get_unique_id())
-	var is_player_one = $"../../../Multiplayer Manager".is_player_one()
-	if is_player_one:
-		$"Player 1 Typing UI".add_child(typing)
-	else:
-		$"Player 2 Typing UI2".add_child(typing)
+	var typing_one = typing_scene.instantiate()
+	typing_one.name = str($"../../../Multiplayer Manager".player_one.ID)
+	$"Player 1 Typing UI".add_child(typing_one)
+	var typing_two = typing_scene.instantiate()
+	typing_two.name = str($"../../../Multiplayer Manager".player_two.ID)
+	$"Player 2 Typing UI2".add_child(typing_two)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
